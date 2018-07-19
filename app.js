23f4dbc0bf6850e655d927ce11222f5c07a1abee
arpadav <arpadav@gmail.com>
2018-07-18 20:15:39 -0400

    links working to /creator

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users')
 var creatorRouter = require('./routes/creator');
 
 var app = express();
@@ -21,8 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/creator', creatorRouter);
-// console.log(creatorRouter);
+app.use('/', creatorRouter);
 
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
