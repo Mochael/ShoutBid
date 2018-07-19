4af995356f5a215a1f5462938e88595ef41116e9
arpadav <arpadav@gmail.com>
2018-07-18 20:15:55 -0400

    routing changes

--- routes/creator.js
+++ routes/creator.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET creator page. */
 router.get('/creator', function(req, res, next) {
   res.render('creator');
 });
