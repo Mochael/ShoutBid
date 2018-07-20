36a704f39311da9679e4d6dd079064bda0d77133
arpadav <arpadav@gmail.com>
2018-07-20 13:46:10 -0400

    NEW functions on creating new data

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var fs = require('fs');
+// var createError = require('http-errors');
 
 var indexRouter = require('./routes/index');
 var creatorRouter = require('./routes/creator');
@@ -17,17 +17,15 @@ var mongoose = require('mongoose');
 // https://www.mongodb.com/download-center#community
 // and stored it locally. not sure if this URL will work for everyone.. but still ill push it
 
-// also michael i see you called your DB local_library123, do u wanna use something like 'shoutbid'?
-// thats what im using below
+const indexpicspath = './public/data/indexpics.json';
 
 const dbname = 'shoutbid';
-var mongoDB = 'mongodb://MisterManMan:PoopPee123Dee@ds143511.mlab.com:43511/' + dbname;
-// var mongoDB = 'mongodb://localhost:27017/' + dbname;
-
-mongoose.connect(mongoDB, {useNewUrlParser: true});
+var mongoDB = 'mongodb://localhost:27017/' + dbname;
 // 27017 for now, MONGODB should have fix soon
 // ^^this is a very recent error MONGODB put out, normally the port number '27017'
 // would not be in this url. it was like beginning of july i saw this on stack exchange.
+
+mongoose.connect(mongoDB, {useNewUrlParser: true});
 
 // Get Mongoose to use the global promise library
 mongoose.Promise = global.Promise;
@@ -45,8 +43,9 @@ db.once('open', function(){
 // var User = require('./models/user');
 // https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/mongoose
 
-var app = express();
+init();
 
+var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -60,8 +59,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/', creatorRouter);
 app.use('/', regRouter);
-//Added by Michael for users
-// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
@@ -76,7 +73,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
@@ -90,4 +86,66 @@ app.use(function(err, req, res, next) {
     exported before it can get to it
 */
 
-module.exports = app;
+module.exports = app
+
+// put whatever in this function to run on startup to initialize new shit
+function init(){
+	var indexpics = JSON.parse(fs.readFileSync(indexpicspath));
+	fs.readdir('./public/indexpics', function(err, files){
+		createNewData({"pics":[]}, indexpics["pics"], files, function(newData){
+			fs.writeFile(indexpicspath, JSON.stringify(newData, null, 4), function(err){
+				if (err) throw err;
+			});
+		}, '"path": "indexpics/~~obj~~"', '"name": "~~obj.split(".")[0]~~"');
+	});
+}
+
+// trying to make this universal, can probably route this instead of putting it into app.js..
+// for now it can stay here
+function createNewData(format, current, updated, cb, ...pushInfo){
+	let newData = format;
+	let key = Object.keys(newData)[0];
+
+	newData[key] = current;
+	compare(current, updated, function(changes){
+		newData[key].forEach(function(obj, i){
+			if(changes.remove.includes(obj.id)){
+				newData[key] = newData[key].filter(function(id){ return id !== obj; });
+			}
+		});
+		changes.add.forEach(function(obj, i){
+			let pushing = '{"id": "' + obj + '", ';
+			pushInfo.forEach(function(param, j){
+				let p = pushInfo[j].split('~~');
+				p.forEach(function(variable, index){
+					if(index % 2 && variable !== '') pushing = pushing + eval(variable);
+					else pushing = pushing + variable;
+				});
+				if(j == pushInfo.length - 1) pushing = pushing + '}'; else pushing = pushing + ', '
+			});
+			newData[key].push(JSON.parse(pushing));
+		});
+	});
+	cb(newData);
+}
+
+// ONLY WORKS WITH DATA THAT HAVE 'ID' AND IS COMPARING TO LIST OF NEW 'ID''S
+// can change to be even MORE universal with big data, where 2 datasets are just compared
+// and uobj and cobj are compared as uobj.id and cobj.id
+function compare(current, updated, cb){
+	let changes = {"exists":[], "add":[], "remove":[]};
+	updated.forEach(function(uobj, i){ // updated obj
+		current.forEach(function(cobj, j){ // current obj
+			if(cobj.id == uobj){
+				changes.exists.push(uobj);
+				changes.add = changes.add.filter(function(id){ return id !== uobj; });
+				changes.remove = changes.remove.filter(function(id){ return id !== cobj.id; });
+			}else if(!changes.exists.includes(uobj) && !changes.add.includes(uobj)){
+				changes.add.push(uobj);
+			}else if(!changes.exists.includes(cobj.id) && !changes.remove.includes(cobj.id)){
+				changes.remove.push(cobj.id);
+			}
+		});
+	});
+	cb(changes);
+}
