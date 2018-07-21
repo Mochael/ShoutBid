1ba80b501b1e683f5bcd4f64c7add12df5a7dae9
arpadav <arpadav@gmail.com>
2018-07-20 20:24:56 -0400

    imported my data stuff as a module, neater app.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var fs = require('fs');
 // var createError = require('http-errors');
 
+var localdata = require('./js/localdata');
+
 var indexRouter = require('./routes/index');
 var creatorRouter = require('./routes/creator');
 var regRouter = require('./routes/registration');
@@ -18,7 +20,6 @@ var mongoose = require('mongoose');
 // and stored it locally. not sure if this URL will work for everyone.. but still ill push it
 
 const indexpicspath = './public/data/indexpics.json';
-
 const dbname = 'shoutbid';
 //var mongoDB = 'mongodb://MisterManMan:PoopPee123Dee@ds143511.mlab.com:43511/' + dbname;
 var mongoDB = 'mongodb://localhost:27017/' + dbname;
@@ -95,59 +96,10 @@ module.exports = app
 function init(){
 	var indexpics = JSON.parse(fs.readFileSync(indexpicspath));
 	fs.readdir('./public/indexpics', function(err, files){
-		createNewData({"pics":[]}, indexpics["pics"], files, function(newData){
+		localdata.createNewData({"pics":[]}, indexpics["pics"], files, function(newData){
 			fs.writeFile(indexpicspath, JSON.stringify(newData, null, 2), function(err){
 				if (err) throw err;
 			});
 		}, '"path": "indexpics/~~obj~~"', '"name": "~~obj.split(".")[0]~~"', '"msg": "null"');
 	});
-}
-
-// trying to make this universal, can probably route this instead of putting it into app.js..
-// for now it can stay here
-function createNewData(format, current, updated, cb, ...pushInfo){
-	let newData = format;
-	let key = Object.keys(newData)[0];
-	newData[key] = current;
-	compare(current, updated, function(changes){
-		newData[key].forEach(function(obj, i){
-			if(changes.remove.includes(obj.id)){
-				newData[key] = newData[key].filter(function(id){ return id !== obj; });
-			}
-		});
-		changes.add.forEach(function(obj, i){
-			let pushing = '{"id": "' + obj + '", ';
-			pushInfo.forEach(function(param, j){
-				let p = pushInfo[j].split('~~');
-				p.forEach(function(variable, index){
-					if(index % 2 && variable !== '') pushing = pushing + eval(variable);
-					else pushing = pushing + variable;
-				});
-				if(j == pushInfo.length - 1) pushing = pushing + '}'; else pushing = pushing + ', '
-			});
-			newData[key].push(JSON.parse(pushing));
-		});
-	});
-	cb(newData);
-}
-
-// ONLY WORKS WITH DATA THAT HAVE 'ID' AND IS COMPARING TO LIST OF NEW 'ID''S
-// can change to be even MORE universal with big data, where 2 datasets are just compared
-// and uobj and cobj are compared as uobj.id and cobj.id
-function compare(current, updated, cb){
-	let changes = {"exists":[], "add":[], "remove":[]};
-	updated.forEach(function(uobj, i){ // updated obj
-		current.forEach(function(cobj, j){ // current obj
-			if(cobj.id == uobj){
-				changes.exists.push(uobj);
-				changes.add = changes.add.filter(function(id){ return id !== uobj; });
-				changes.remove = changes.remove.filter(function(id){ return id !== cobj.id; });
-			}else if(!changes.exists.includes(uobj) && !changes.add.includes(uobj)){
-				changes.add.push(uobj);
-			}else if(!changes.exists.includes(cobj.id) && !changes.remove.includes(cobj.id)){
-				changes.remove.push(cobj.id);
-			}
-		});
-	});
-	cb(changes);
 }
