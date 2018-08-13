59ae541e4eba914714c4f7b63236557dd92c052d
Michael Kronovet <michaelkronovet@Michaels-MacBook-Pro.local>
2018-08-13 17:13:24 -0400

    mooeee

--- routes/users.js
+++ routes/users.js
@@ -125,8 +125,7 @@ router.post('/register', function (req, res) {
       req.logout();
   
       req.flash('success_msg', 'You are logged out');
-  
-      res.redirect('/login');
+      res.redirect('/');
   });
   
   module.exports = router;
